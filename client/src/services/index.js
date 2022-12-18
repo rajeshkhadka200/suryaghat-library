7f257d6ca415ec6bc03d513eddb1e4a01d09c776
dev.saran <zireavai8@gmail.com>
2022-12-18 20:22:55 +0545

    server uptodate

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getApi = axios.create({
-  baseURL: "http://localhost:3001",
-  // baseURL: "https://api.suryaghatlibrary.com",
+  // baseURL: "http://localhost:3001",
+  baseURL: "https://api.suryaghatlibrary.com",
 });
